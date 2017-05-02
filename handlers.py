f9794286deae02f95b112a31480516774760eae3
luoyingtian <yt.luo@idiaoyan.com>
2017-05-02 13:18:56 +0800

    display paypal button

--- handlers.py
+++ handlers.py
@@ -3,4 +3,4 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.render('index.html')
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 # encoding=utf8
 
+import os
 import sys
 import tornado.ioloop
 import tornado.web
 import handlers
 
 
-application = tornado.web.Application([
+url_conf = [
     (r"/", handlers.MainHandler),
-])
+]
+
+application = tornado.web.Application(
+    handlers=url_conf,
+    template_path=os.path.join(os.path.dirname(__file__), "templates"),
+)
 
 if __name__ == "__main__":
     PORT = sys.argv[1]
