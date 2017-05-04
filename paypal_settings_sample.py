8566e41c764a509118b9bb1c789701619cbf3425
luoyingtian <yt.luo@idiaoyan.com>
2017-05-04 10:08:45 +0800

    add paypal settings sample

---
+++ paypal_settings_sample.py
@@ -0,0 +1,9 @@
+# encoding=utf8
+
+USE_SANDBOX = True
+BUSINESS_EMAIL_SANDBOX = ''
+BUSINESS_EMAIL_LIVE = ''
+NOTIFY_URL_SANDBOX = 'http://xx.com/ipn/sandbox'
+NOTIFY_URL_LIVE = 'http://xx.com/ipn/live'
+VERIFY_URL_LIVE = 'https://ipnpb.paypal.com/cgi-bin/webscr'
+VERIFY_URL_SANDBOX = 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr'
